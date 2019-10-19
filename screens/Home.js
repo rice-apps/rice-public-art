9a72af53d69360c47af152db978271ad42ce8d31
Adam Zawierucha <zawiedev@gmail.com>
2019-10-19 13:52:07 -0500

    Added art components to home page

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import Art from '../source/Art.js';
+import Piece from '../source/Piece.js';
 
 export default class HomeScreen extends React.Component {
     static navigationOptions = {
@@ -8,12 +10,24 @@ export default class HomeScreen extends React.Component {
     render() {
       const {navigate} = this.props.navigation;
       return (
-        
-        <Button
-          title="Go to Announcements!"
-          onPress={() => navigate('Announce', {name: 'Jane'})}
-        />
+        <View style={styles.art_container}>
+            <Art>
+                <Piece title={"Berlin Wall"} desc={"This is a wall"} image={require("../images/wall.png")}> </Piece>
+                <Piece title={"Skyspace"} desc={"This is art"} image={require("../images/skyspace.jpg")} > </Piece>  
+            </Art>
+            <Button
+                title="Go to Announcements!"
+                onPress={() => navigate('Announce', {name: 'Jane'})}
+            />
+        </View>
       );
     }
   }
   
+  const styles = StyleSheet.create({
+    art_container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
