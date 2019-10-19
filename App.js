c9d240be79fc6522a2c8526cc4b8f210388245bc
Ananya Vaidya <akv4@rice.edu>
2019-10-19 13:57:37 -0500

    added react-navigation-tabs

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
+//added bottom tab navigator
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 //Screens
 import HomeScreen from './screens/Home'
