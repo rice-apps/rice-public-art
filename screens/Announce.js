169a9d992ae60edd6cfa49dc0753e7afbc4478c0
Adam Zawierucha <zawiedev@gmail.com>
2019-10-19 14:11:49 -0500

    centered test announcement

--- screens/Announce.js
+++ screens/Announce.js
@@ -4,8 +4,17 @@ import { StyleSheet, Text, View, Image, Button } from 'react-native';
 export default class AnnounceScreen extends React.Component {
     render() {
       return (
-        <Text> Moody is on fire! </Text>
+        <View style={styles.art_container}>
+          <Text> Moody is on fire! </Text>
+        </View>
       );
     }
   }
 
+const styles = StyleSheet.create({
+    art_container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+});
