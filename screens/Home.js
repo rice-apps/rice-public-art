109239806c54fe380ccb31abb35c6536d17dfe4b
Adam Zawierucha <zawiedev@gmail.com>
2019-10-21 20:57:18 -0500

    added relevant comments

--- screens/Home.js
+++ screens/Home.js
@@ -4,6 +4,7 @@ import Art from '../source/Art.js';
 import Piece from '../source/Piece.js';
 
 export default class HomeScreen extends React.Component {
+  // Set default state for Homescreen (no data, and loading)
   constructor(props) {
     super(props);
     this.state = {
@@ -11,10 +12,13 @@ export default class HomeScreen extends React.Component {
       dataSource:[]
      };
    }
+   // Fires when componenet is initially set/mounted
     componentDidMount(){
+      //Make a fetch call
       fetch('http://moody-backend.herokuapp.com/moodyArt/allArt', {method: 'GET'})
-        .then(response => response.json())
-        .then((responseJson)=> {
+        .then(response => response.json()) // Get json of response
+        .then((responseJson)=> { 
+            // Adjust state to reflect loaded status / store data from response
             this.setState({
             loading: false,
             data: responseJson
@@ -23,14 +27,18 @@ export default class HomeScreen extends React.Component {
         .catch(error=>console.log(error)) //to catch the errors if any
     }
     render() {
-      console.log(this.state.loading);
+      // Check if data is loaded
       if(this.state.loading){
+        // Display something to inform user data is loading
         return( 
           <View style={styles.loader}> 
             <Text> Loading... </Text>
           </View>
       )} else {
+        // We got the data
+          // Here we have to do somoething with this.state.data to reflect in the return statement
         console.log("loaded data:",this.state.data);
+        // Return display formating data into Piece componenets
         return(
           <View style={styles.container}>
             <Text> Done! </Text>
@@ -40,6 +48,7 @@ export default class HomeScreen extends React.Component {
     }
   }
   
+  // Style
   const styles = StyleSheet.create({
     container: {
       flex: 1,
