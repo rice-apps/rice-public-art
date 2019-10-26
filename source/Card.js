3f8eb62938ab43174c17067b6978b25ad5bff80d
Adam Zawierucha <zawiedev@gmail.com>
2019-10-26 14:49:11 -0500

    a comment about https

--- source/Card.js
+++ source/Card.js
@@ -3,13 +3,13 @@ import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default class Card extends React.Component {
     render() {
+      // Remove "https:", eventually (image componenet)
         return (
           <View style={styles.container}>
             <Text> {this.props.name} </Text>
             <Text> {this.props.description} </Text>
             <Image style={{width: 250, height: 250}} source={{uri: "https:"+this.props.image}}/>
             <Text> {this.props.location} </Text>
-            
           </View>
         );
       }
