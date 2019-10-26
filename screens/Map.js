ead9e5e72814c6bdc8a11a9adc5ec46df98b9d65
Ryan Knightly <reknightly@gmail.com>
2019-10-26 12:41:41 -0500

    Add map to Map screen

--- screens/Map.js
+++ screens/Map.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Image, Button, Dimensions } from 'react-native';
 
 export default class MapScreen extends React.Component {
     render() {
       return (
-        <View style={styles.art_container}>
-          <Text> Check out our map! </Text>
+        <View style={styles.container}>
+          <MapView style={styles.mapStyle} />
         </View>
       );
     }
   }
 
-const styles = StyleSheet.create({
-    art_container: {
+  const styles = StyleSheet.create({
+    container: {
       flex: 1,
+      backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
     },
-});
+    mapStyle: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    },
+  });
