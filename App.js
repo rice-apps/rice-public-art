28cce5e75748ef9dfac2c957ccdb4ba996bc0fdf
Ryan Knightly <reknightly@gmail.com>
2019-10-26 13:59:38 -0500

    Add tabbar icons

--- App.js
+++ App.js
@@ -11,10 +11,52 @@ import HomeScreen from './screens/Home'
 import MapScreen from './screens/Map'
 import AnnounceScreen from './screens/Announce.js'
 
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Map: MapScreen,
-  Announcements: AnnounceScreen,
+// const TabNavigator = createBottomTabNavigator({
+//   Home: HomeScreen,
+//   Map: MapScreen,
+//   Announcements: AnnounceScreen,
+// });
+
+const TabNavigator = createBottomTabNavigator ({
+  Home:{
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarIcon: (focused, tintColor) => (
+        <Image 
+          style={{ width: 35, height: 35 }}
+          source={require('./images/mapIcon.png')} 
+          tintColor={tintColor}/>
+      )
+    }
+  },
+  Map:{
+    screen: MapScreen,
+    navigationOptions:{
+      tabBarIcon: (focused, tintColor) => (
+        <Image 
+          style={{ width: 35, height: 35 }}
+          source={require('./images/mapIcon.png')} 
+          tintColor={tintColor}/>
+      )
+    }
+  },
+  Announcements:{
+    screen: AnnounceScreen,
+    navigationOptions:{
+      tabBarIcon: (focused, tintColor) => (
+        <Image 
+          style={{ width: 35, height: 35 }}
+          source={require('./images/mapIcon.png')} 
+          tintColor={tintColor}/>
+      )
+    }
+  }
+}, {
+  tabBarOptions: { 
+    showLabel: false,
+    activeTintColor: '#000000',
+    inactiveTintColor: '#555555'
+  }
 });
 
 export default createAppContainer(TabNavigator);
