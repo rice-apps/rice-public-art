dd3513d812f282c5abe62b8e30fec1d819e32064
Ananya Vaidya <akv4@rice.edu>
2019-11-16 15:02:13 -0600

    adjusted art page with touchable highlight and borders

--- components/Card.js
+++ components/Card.js
@@ -5,11 +5,12 @@ export default class Card extends React.Component {
     render() {
         return (
           <TouchableHighlight style={styles.touchable}
+            underlayColor="transparent"
             style = {styles.container}
             onPress={() => 
               this.props.navigation.navigate('Details',this.props)
             }>
-            <ImageBackground style={{borderRadius:100, height: 200, width:"100%", marginTop:10}}  imageStyle={{ borderRadius: 25, width:"100%"}} source={{uri: this.props.image}}>
+            <ImageBackground style={{borderRadius:100, height: 200, width:"100%", marginTop:10}}  imageStyle={{ borderRadius: 10, width:"100%"}} source={{uri: this.props.image}}>
               <Text style={{textAlign: "left", color:"white", textTransform: "uppercase", fontSize: 30, marginTop:10, marginLeft:10}}> {this.props.name} </Text>
               {/* <Text> {this.props.description} </Text> */}
               <Text> {this.props.location} </Text>
