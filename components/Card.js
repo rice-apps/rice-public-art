7b8683eb0ceccdb75ba900e79c24674fe9ff56d1
Ryan Knightly <reknightly@gmail.com>
2019-11-23 13:30:04 -0600

    Add distance to the bottom of card

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, ImageBackground, StyleSheet, TouchableHighlight} from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, TouchableHighlight} from 'react-native';
 
 export default class Card extends React.Component {
     render() {
@@ -12,7 +12,9 @@ export default class Card extends React.Component {
             }>
             <ImageBackground style={{borderRadius:100, height: 200, width:"100%", marginTop:10}}  imageStyle={{ borderRadius: 10, width:"100%"}} source={{uri: this.props.image}}>
               <Text style={{textAlign: "left", color:"white", textTransform: "uppercase", fontSize: 30, marginTop:10, marginLeft:10}}> {this.props.name} </Text>
-              {/* <Text> {this.props.description} </Text> */}
+              <View style={{flex: 1, justifyContent: 'flex-end'}}>
+                <Text style={{textAlign: "right", color:"white", fontSize: 20, marginBottom:10, marginRight:10}}>10 min. away</Text>
+              </View>
               <Text> {this.props.location} </Text>
             </ImageBackground>
           </TouchableHighlight>
