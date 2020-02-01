81ffa73aedeec10ad44ede01f652b645c5336a3b
Adam Zawierucha <zawiedev@gmail.com>
2020-02-01 14:08:28 -0600

    carousel festival

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 
-let data = ["JANUARY", "FEBRUARY", "MARCH"];
+let data = ["JANUARY", "FEBRUARY", "MARCH","APRIL","MAY","JUNE","JULY","AUGUST","OCTOBER","NOVEMBER","DECEMBER"];
 
 export default class TextCarousel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.carousel = (<Carousel 
+                ref={(c) => { this._carousel = c; }}
+                data={data}
+                renderItem={this._renderItem}
+                sliderWidth={400}
+                itemWidth={120}
+            />
+        )
 
+    }
     _renderItem = ({item, index}) => {
         return (
             <View >
-                <Text >{ item }</Text>
+                <Button title = {item}
+                    onPress = {()=>this._carousel.snapToItem(index, animated = true, fireCallback = true)}
+                ></Button>
             </View>
         );
     }
-
     render () {
         return (
-            <Carousel 
-              ref={(c) => { this._carousel = c; }}
-              data={data}
-              renderItem={this._renderItem}
-              sliderWidth={400}
-            itemWidth={100}
-            />
+            this.carousel
         );
     }
+
+
 }
