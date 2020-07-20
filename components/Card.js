15770da5ece77f752a5702e8073e5a2b6f23c39e
Adam Zawierucha <zawiedev@gmail.com>
2020-07-20 15:52:34 -0700

    adjusted card gradient & textsize

--- components/Card.js
+++ components/Card.js
@@ -15,10 +15,10 @@ export default class Card extends React.Component {
             }>
             <ImageBackground style={{borderRadius:100, height: 200, width:"100%", marginTop:10}}  imageStyle={{ borderRadius: 10, width:"100%"}} source={{uri: this.props.image}}>
               <LinearGradient
-                colors={['rgba(0, 0, 0, .95)', 'rgba(0, 0, 0, .75)', 'rgba(0, 0, 0, .5)','rgba(0, 0, 0, 0)']}
+                colors={['rgba(0, 0, 0, .95)', 'rgba(0, 0, 0, .7)', 'rgba(0, 0, 0, .4)','rgba(0, 0, 0, 0)']}
                 style={{ padding: 15, borderRadius: 5 }}
               > 
-              <Text style={{textAlign: "left", fontFamily: "aktiv-grotesk-regular", color:"white", textTransform: "uppercase", fontSize: 30, marginTop:-10, marginLeft:-5}}> {truncate(this.props.name)} </Text>
+              <Text style={{textAlign: "left", fontFamily: "aktiv-grotesk-regular", color:"white", textTransform: "uppercase", fontSize: 25, marginTop:-10, marginLeft:-5}}> {truncate(this.props.name,20)} </Text>
               </LinearGradient>
               <LinearGradient
                 colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, .1)', 'rgba(0, 0, 0, .5)', 'rgba(0, 0, 0, .9)']}
