7ae8b9a3d7c052faad4fd1f5d994f7df7d184d5e
jacqw8 <jacquelinewu88@gmail.com>
2021-07-27 00:30:25 -0400

    remove filter and cards from artist tab

--- screens/Artist.js
+++ screens/Artist.js
@@ -21,27 +21,21 @@ class ArtistScreen extends React.Component {
 
         return {
             headerLeft: <Topbar text="Artist" />,
-            headerRight: () => (
-                <Button
-                    onPress={navigation.getParam('showFilter')}
-                    title="Filter"
-                    color="#fff"
-                />
-            ),
+
             headerStyle: {
                 backgroundColor: LIGHT_BLUE,
             },
         };
     };
 
-    componentWillMount() {
-        this.props.navigation.setParams({ showFilter: this._showFilter });
-    }
+    // componentWillMount() {
+    //     this.props.navigation.setParams({ showFilter: this._showFilter });
+    // }
 
-    _showFilter = () => {
-        this.setState({ showFilterMenu: true });
-        //console.log("Showing filter menu");
-    }
+    // _showFilter = () => {
+    //     this.setState({ showFilterMenu: true });
+    //     //console.log("Showing filter menu");
+    // }
 
     // Set default state for Homescreen (no data, and loading)
     constructor(props) {
@@ -49,122 +43,26 @@ class ArtistScreen extends React.Component {
         this.state = {
             loading: true,
             data: [],
-            userLocation: null,
-            showFilterMenu: false,
-            filters: {
-                sculpture: false,
-                painting: false,
-                film: false,
-                photography: false,
-                installation: false,
-                drawingAndPrints: false,
-                indoors: false,
-                outdoors: false,
-            }
         };
     }
 
     // Fires when componenet is initially set/mounted
     componentDidMount() {
-        //Make a fetch call
-        fetch('https://moody-backend-273918.uc.r.appspot.com/campusArt/allArt', { method: 'GET' })
-            .then(response => response.json()) // Get json of response
-            .then((responseJson) => {
-                // Adjust state to reflect loaded status / store data from response
-
-                this.setState({
-                    loading: false,
-                    data: responseJson.data
-                })
-            })
-            .catch(error => console.log(error)) //to catch the errors if any
-
-        navigator.geolocation.getCurrentPosition((position) => {
-            var lat = parseFloat(position.coords.latitude)
-            var long = parseFloat(position.coords.longitude)
-
-            this.setState({
-                userLocation: {
-                    latitude: lat,
-                    longitude: long
-                }
-            })
-        },
-            (error) => console.log(JSON.stringify(error)),
-            { enableHighAccuracy: false, timeout: 20000, maximumAge: 1000 });
+        this.setState({
+            loading: false
+        })
     }
 
     generateCards() {
         var cards = this.state.data.map((content, i) =>
-            this.shouldShowArtWithFilter(content) ?
-                <Card
-                    key={'card' + i}
-                    name={content.name}
-                    description={content.description}
-                    image={content.image}
-                    artist={content.artist}
-                    location={content.location}
-                    year={content.year}
-                    index={i} // !!!!!!!!CAUTION: This might be deprecated if filtering is added!!!!!!!!
-                    // Assume a 20 min. per mile walking speed
-                    // This is on the high end because we are using a straight line approximation for the route
-                    // which underestimates the total distance
-                    minutes={this.state.userLocation != null ?
-                        Math.round(distance(this.state.userLocation.latitude, this.state.userLocation.longitude,
-                            content.location.lat, content.location.lon) * 20)
-                        : null}
-                    navigation={this.props.navigation}
-                /> : null)
-        if (this.state && this.state.userLocation && this.state.userLocation.latitude && this.state.userLocation.longitude) {
-            var myloc = { lat: this.state.userLocation.latitude, lon: this.state.userLocation.longitude }
-            cards.sort((a, b) => {
-                if (a != null & b != null) {
-                    var aloc = a.props.location; var bloc = b.props.location;
-                    var d1 = distance(aloc.lat, aloc.lon, myloc.lat, myloc.lon)
-                    var d2 = distance(bloc.lat, bloc.lon, myloc.lat, myloc.lon)
-                    return d1 - d2
-                }
-            })
-        }
+            <Card
+                key={'card' + i}
+                index={i} // !!!!!!!!CAUTION: This might be deprecated if filtering is added!!!!!!!!
+            />)
+
         return cards;
     }
-    toggleFilter(filterName) {
-        let filters = this.state.filters
-        var v = !filters[filterName]
-        //Reset filters
-        Object.keys(filters).forEach((key) => filters[key] = false)
-        filters[filterName] = v
-        if (this.generateCards().filter(x => x != null).length == 0) {
-            //Alert user
-            Alert.alert("No content", "Try selecting different filters");
-            filters[filterName] = false;
-        }
-        this.setState({ filters: filters })
-    }
 
-    shouldShowArtWithFilter(content) {
-        //console.log("********** FILTERING ***********")
-        //console.log(content);
-        if (content.indoors == false) {
-            content.outdoors = true;
-        }
-        const filters = ['sculpture', 'painting', 'film', 'photography', 'installation', 'drawingAndPrints', 'indoors', 'outdoors'];
-        var passedFilter = false
-        var allFiltersTrue = true
-        var allFiltersFalse = true
-        filters.forEach(filter => {
-            //console.log(filter, this.state.filters[filter], content[filter]);
-            if (this.state.filters[filter] && content[filter]) {
-                passedFilter = true;
-            }
-            if (!this.state.filters[filter]) allFiltersTrue = false;
-            if (this.state.filters[filter]) allFiltersFalse = false;
-        })
-        // don't exclude anything if nothing is checked
-        // or if nothing is filtered out
-        if (allFiltersFalse || allFiltersTrue) return true;
-        return passedFilter;
-    }
 
     render() {
         // Check if data is loaded
@@ -185,64 +83,7 @@ class ArtistScreen extends React.Component {
                     <ScrollView style={styles.scrollView}>
                         {this.generateCards()}
                     </ScrollView>
-                    <Modal
-                        isVisible={this.state.showFilterMenu}
-                        onSwipeComplete={() => this.setState({ showFilterMenu: false })}
-                        swipeDirection={['down']}
-                    >
-                        <View style={styles.filterMenu}>
-                            <Text style={styles.filterMenuTitle}>Filter Art</Text>
-                            <CheckBox
-                                title='Sculpture'
-                                checked={this.state.filters.sculpture}
-                                onPress={() => this.toggleFilter('sculpture')}
-                                containerStyle={{ width: 225 }}
-                            />
-                            <CheckBox
-                                title='Painting'
-                                checked={this.state.filters.painting}
-                                onPress={() => this.toggleFilter('painting')}
-                                containerStyle={{ width: 225 }}
-                            />
-                            <CheckBox
-                                title='Film'
-                                checked={this.state.filters.film}
-                                onPress={() => this.toggleFilter('film')}
-                                containerStyle={{ width: 225 }}
-                            />
-                            <CheckBox
-                                title='Photography'
-                                checked={this.state.filters.photography}
-                                onPress={() => this.toggleFilter('photography')}
-                                containerStyle={{ width: 225 }}
-                            />
-                            <CheckBox
-                                title='Installation'
-                                checked={this.state.filters.installation}
-                                onPress={() => this.toggleFilter('installation')}
-                                containerStyle={{ width: 225 }}
-                            />
-                            <CheckBox
-                                title='Drawing and Prints'
-                                checked={this.state.filters.drawingAndPrints}
-                                onPress={() => this.toggleFilter('drawingAndPrints')}
-                                containerStyle={{ width: 225 }}
-                            />
-                            <CheckBox
-                                title='Indoors'
-                                checked={this.state.filters.indoors}
-                                onPress={() => this.toggleFilter('indoors')}
-                                containerStyle={{ width: 225 }}
-                            />
-                            <CheckBox
-                                title='Outdoors'
-                                checked={this.state.filters.outdoors}
-                                onPress={() => this.toggleFilter('outdoors')}
-                                containerStyle={{ width: 225 }}
-                            />
-                            <Button testID={'close-button'} onPress={() => this.setState({ showFilterMenu: false })} title="Close" />
-                        </View>
-                    </Modal>
+
                 </View>
             );
         }
@@ -267,17 +108,5 @@ const styles = StyleSheet.create({
         marginLeft: "auto",
         marginRight: "auto",
         width: "95%"
-    },
-    filterMenu: {
-        backgroundColor: 'white',
-        padding: 22,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 4,
-        borderColor: 'rgba(0, 0, 0, 0.1)',
-    },
-    filterMenuTitle: {
-        fontSize: 20,
-        marginBottom: 12,
     },
 });
