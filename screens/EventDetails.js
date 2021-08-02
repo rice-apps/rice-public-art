4e47a74895f8954714d181722ca9d5ff9662101b
bzx24 <brianxu16@gmail.com>
2021-08-02 13:09:29 -0500

    adding events functionality

--- screens/EventDetails.js
+++ screens/EventDetails.js
@@ -4,6 +4,9 @@ import Topbar from '../components/Topbar.js';
 import { Dimensions } from 'react-native';
 import Image from 'react-native-scalable-image';
 import { COLORS, LIGHT_GREEN } from '../COLORS.js';
+import { Button } from 'react-native-elements';
+import * as Calendar from 'expo-calendar'
+import * as Permissions from 'expo-permissions';
 
 export default class EventDetailsScreen extends React.Component {
     
@@ -19,6 +22,32 @@ export default class EventDetailsScreen extends React.Component {
         },
       }
     }
+
+    handlePress = async () => {
+      //Getting Permissions
+      const statusCal = await Permissions.askAsync(Permissions.CALENDAR);
+      const statusRem = await Permissions.askAsync(Permissions.REMINDERS);
+  
+      if (statusCal.status === 'granted' && statusRem.status === 'granted') {
+        //"Default" Calendar determined as first calendar
+        const { id } = await getDefaultCalendar()
+        const calEvent = await Calendar.createEventAsync(id,
+          {
+            title: this.getParam("title"),
+            startDate: this.getParam("date"),
+            endDate: new Date(this.getParam("date").getTime() + 60*60000),
+            timeZone: 'US/Central',
+            location: this.getParam("location"),
+            availability: this.getParam("time")
+          })
+          console.log("Event Added to Calendar")
+          alert('This event has been added to your calendar.')
+      }
+      else{
+        alert('Cannot add event to calendar without calendar permissions.')
+      }
+    }
+
     render() {
       
       return (
@@ -42,14 +71,44 @@ export default class EventDetailsScreen extends React.Component {
                 <Text style={styles.secondaryText}>{this.getParam('time')}</Text>
                 <Text style={styles.secondaryText}>{this.getParam('location')}</Text>
               </View>
+              <View style={styles.button}>
+                <Button
+                    title="Add to Calendar"
+                    onPress={this.handlePress}
+                    buttonStyle={{
+                      padding: 10,
+                    }}
+                />
+              </View>
             </View>
             <View style = {styles.innerView}>
               <Text style = {styles.description}>{this.getParam("description","default desc")}</Text>
+            </View>
+            <View style={styles.button}>
+              <Button
+                  title="Add to Calendar"
+                  onPress={this.handlePress}
+                  buttonStyle={{
+                    padding: 10,
+                    marginTop: 20,
+                    width: 170
+                  }}
+              />
             </View>
         </ScrollView>  
       );
     }
   }
+
+async function getDefaultCalendar() {
+  const sources = await Calendar.getCalendarsAsync();
+  const defaultCalendar = sources[0];
+  if (!defaultCalendar) {
+    alert('No calendars availabe.')
+    return;
+  }
+  return defaultCalendar;
+}
 
 const styles = StyleSheet.create({
   scrollView: {
@@ -96,4 +155,8 @@ const styles = StyleSheet.create({
     marginTop: -10,
     marginLeft: 10,
   },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
